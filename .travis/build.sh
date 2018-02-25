56afca3395c332bfc5862b2d11fc47519871638b
Alexandre Macabies <web+git@zopieux.com>
2018-02-26 00:07:06 +0100

    travis: test docker

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,10 +2,29 @@
 
 set -ex
 
-cd deployment/pkg/camisole-languages
+sudo pip install -e .
 
-make PKGBUILD
-make .SRCINFO
+pushd /tmp
+ git clone https://aur.archlinux.org/cower.git 
+ pushd cower
+  makepkg -fsri --noconfirm --needed --asdeps --skippgpcheck
+ popd
+ git clone https://aur.archlinux.org/pacaur.git 
+ pushd pacaur
+  makepkg -fsri --noconfirm --needed --asdeps --skippgpcheck
+ popd
+popd
 
-makepkg -fcsi
+export EDITOR=vi
+export VISUAL=vi
+pacaur -S --noconfirm --noedit isolate-git
+
+pushd deployment/pkg/camisole-languages
+export PATH=/usr/bin
+
+make PKGBUILD .SRCINFO
+makepkg -fsri --noconfirm --needed --asdeps --skippgpcheck
+
+which camisole
+camisole -l debug test -vv
 
